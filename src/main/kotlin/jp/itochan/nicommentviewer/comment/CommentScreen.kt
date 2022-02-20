aad05201b5fbccc8847de8e05727ff7dbc159c4a
Kazunori Jo <519604+itochan@users.noreply.github.com>
2022-02-20 17:09:45 +0900

    Add LazyColumn

--- src/main/kotlin/jp/itochan/nicommentviewer/comment/CommentScreen.kt
+++ src/main/kotlin/jp/itochan/nicommentviewer/comment/CommentScreen.kt
@@ -2,6 +2,10 @@ package jp.itochan.nicommentviewer.comment
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import jp.itochan.nicommentviewer.api.client.NiClient
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -19,18 +24,27 @@ import kotlinx.coroutines.launch
 fun CommentScreen() {
     Surface {
         var channelId by remember { mutableStateOf("") }
-        Column {
-            TextField(
-                value = channelId,
-                onValueChange = { channelId = it }
-            )
-            Button(onClick = {
-                GlobalScope.launch {
-                    val webSocketUrl = NiClient.getWebSocketUrl(channelId)
-                    webSocketUrl?.let { NiClient.watch(it) }
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row {
+                TextField(
+                    value = channelId,
+                    onValueChange = { channelId = it }
+                )
+                Button(onClick = {
+                    GlobalScope.launch {
+                        val webSocketUrl = NiClient.getWebSocketUrl(channelId)
+                        webSocketUrl?.let { NiClient.watch(it) }
+                    }
+                }) {
+                    Text(text = "Get")
                 }
-            }) {
-                Text(text = "Get")
+            }
+            LazyColumn {
+                repeat(1000) {
+                    item {
+                        Text(it.toString())
+                    }
+                }
             }
         }
     }
