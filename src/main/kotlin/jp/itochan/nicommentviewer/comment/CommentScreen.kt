1d90f44e146f0f03949afe9be59895b180e23454
Kazunori Jo <519604+itochan@users.noreply.github.com>
2022-02-20 18:00:04 +0900

    Show comments

--- src/main/kotlin/jp/itochan/nicommentviewer/comment/CommentScreen.kt
+++ src/main/kotlin/jp/itochan/nicommentviewer/comment/CommentScreen.kt
@@ -7,25 +7,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import jp.itochan.nicommentviewer.api.client.NiClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @Composable
 fun CommentScreen() {
     Surface {
         var channelId by remember { mutableStateOf("") }
+        val comments = remember { mutableStateListOf<String>() }
+        val commentsListState = rememberLazyListState()
         Column(modifier = Modifier.fillMaxSize()) {
             Row {
                 TextField(
@@ -35,8 +43,9 @@ fun CommentScreen() {
                 Button(
                     onClick = {
                         GlobalScope.launch {
-                            val webSocketUrl = NiClient.getWebSocketUrl(channelId)
-                            webSocketUrl?.let { NiClient.watch(it) }
+                            NiClient.connect(channelId).collect { (date, comment) ->
+                                comments.add("$date $comment")
+                            }
                         }
                     },
                     modifier = Modifier.padding(start = 8.dp)
@@ -45,12 +54,14 @@ fun CommentScreen() {
                 }
             }
             LazyColumn(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                state = commentsListState
             ) {
-                repeat(1000) {
-                    item {
-                        Text(it.toString())
-                    }
+                itemsIndexed(comments) { _, item ->
+                    Text(text = item)
+                }
+                CoroutineScope(Dispatchers.Main).launch {
+                    commentsListState.scrollToItem(comments.size)
                 }
             }
         }
